c28c1e49bd16a4dc3e3b49417cd4339c0a3e14ef
einarhjorleifsson <einar.hjorleifsson@gmail>
2013-11-03 10:53:50 +0000

    add some more

---
+++ R/fishvise.R
@@ -0,0 +1,10 @@
+#' @name fishvise-package
+#' @aliases fishvise
+#' @aliases fishviseLibrary
+#' @docType package
+#' @title   Title Fishvice package
+#' @description More about what it does (maybe more than one line)
+#' @details Version 1.0: 14 December 2010
+#' @author Various folks at MRI
+#' @seealso link to other documents
+NULL
