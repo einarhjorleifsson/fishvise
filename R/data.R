0e6f8ecede3c2988fa7d3e2dd7ab63ba8b32645b
einarhjorleifsson <einar.hjorleifsson@gmail>
2013-11-21 12:54:18 +0000

    added hcr-stimulator

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' @name cn_keys
+#' @title XX
+#' @description A \emph{list} column matching keys
+#' @docType data
+#' @usage Some usage
+#' @author Einar Hjorleifsson
+NULL
---
+++ R/do_ggplot.R
@@ -0,0 +1,21 @@
+#' @title Do a ggplot
+#' 
+#' @description xx
+#' 
+#' @export
+#' 
+#' @param x xx
+#' @param d xx
+#' 
+do.ggplot <- function(x,d) {
+  q05 <- q10 <- q16 <- q50 <- q84 <- q90 <- q95 <- value <- variable <- NULL
+  gg.plot <- ggplot(x,aes(variable)) +  
+    geom_ribbon(aes(ymin=q05,ymax=q95),fill='grey',alpha=1/2) +
+    geom_ribbon(aes(ymin=q10,ymax=q90),fill='grey',alpha=1/2) +
+    geom_ribbon(aes(ymin=q16,ymax=q84),fill='grey',alpha=1/2) +
+    geom_line(data=d,aes(variable,value,group=iter),alpha = 0.05,col='red') +
+    geom_line(aes(y=q50),col='red',lwd=1) +
+    geom_line(aes(y=mean),col='blue',lwd=1)
+  
+  return(gg.plot)
+}
