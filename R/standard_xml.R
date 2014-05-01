19d7db3cc64ef3104d2b9749c4881ec5e2b36698
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2014-05-01 08:11:18 +0000

    some git solving

--- R/standard_xml.R
+++ R/standard_xml.R
@@ -1,38 +1,75 @@
 #' @title rvk2icesxml
 #' 
-#' @description Converts data.frame to ices xml format for standard graphs.
-#' Note that currently takes only data by year. Need to implement for the yield
-#' per recruit data.
+#' @description Converts data.frame to ices xml format for standard graphs. The
+#' function is a brute force, inefficient and unelegant coding. The primary
+#' aim was just to get something operational for those living in the R environment.
+#' 
+#' The current version of the function does not have any error checking on the input data.
+#' 
+#' This function and updates resides in the fishvise-package located in the authors
+#' github repository. The package can be installed by the following R-commands:
+#' \code{library(devtools)}
+#' \code{install_github("fishvise","einarhjorleifsson")}
+#' \code{library(fishvise)}
+#' 
+#' @author einar.hjorleifsson@gmail.com
 #' 
 #' @export
 #' 
-#' @param rby A data frame containing stock summary data. Required column names
-#' are:
+#' @param rby \code{data.frame} containing stock summary data. Column names
+#' besides the Year column which is required can be any of the following:
 #' \itemize{
-#'  \item \emph{year} - year
-#'  \item \emph{r} - recruitment
-#'  \item \emph{bio} - total or reference biomass
-#'  \item \emph{ssb} - spawning stock biomass
-#'  \item \emph{y} - yield,
-#'  \item \emph{f} - fishing mortality.
+#'  \item \emph{Year} - Year
+#'  \item \emph{Low_Recruitment} - XX
+#'  \item \emph{Recruitment} - XXX
+#'  \item \emph{High_Recruitment} - XXX
+#'  \item \emph{Low_TBiomass} - XXX
+#'  \item \emph{TBiomass} - XXX
+#'  \item \emph{High_TBiomass} - XXX
+#'  \item \emph{Low_SSB} - XXX
+#'  \item \emph{SSB} - XXX
+#'  \item \emph{High_SSB} - XXX
+#'  \item \emph{Catches} - XXX
+#'  \item \emph{Landings} - XXX
+#'  \item \emph{Discards} - XXX
+#'  \item \emph{IBC} - XXX
+#'  \item \emph{Unallocated_Removals} - XXX
+#'  \item \emph{YieldSSB} - XXX
+#'  \item \emph{Low_F} - XXX
+#'  \item \emph{F} - XXX
+#'  \item \emph{High_F} - XXX
+#'  \item \emph{F_Landings} - XXX
+#'  \item \emph{F_Discards} - XXX
+#'  \item \emph{F_IBC} - XXX
+#'  \item \emph{F_Unallocated} - XXX
+#'  \item \emph{SoP} - XXX
+#'  \item \emph{Custom1} - XXX
+#'  \item \emph{Custom2} - XXX
+#'  \item \emph{Custom3} - XXX
+#'  \item \emph{Custom4} - XXX
+#'  \item \emph{Custom5} - XXX
 #' }
 #' @param FishStock A character vector containing stock "name". e.g. "cod-farp", "cod-iceg".
 #' @param AssessmentYear An integer representing assessment year
 #' @param RecruitmentAge An integer representing recruitment age
 #' @param FAge A character vector containg reference fishing moralites, e.g. "F5-10"
-#' @param rba \\code{data.frame} containing yield per recruit input. If missing it is
-#' ignored. If supplied required column names are:
+#' @param rba \code{data.frame} containing yield per recruit input. If missing it is
+#' ignored. Column names besides the Age column which is required can be any of
+#' the following:
 #' \itemize{
-#'  \item \emph{age} - age
+#'  \item \emph{Age} - age
 #'  \item \emph{M} - natural mortality
 #'  \item \emph{Mat} - maturity
 #'  \item \emph{PF} - partial fishing mortality before spawning
 #'  \item \emph{PM} - partial natural mortality before spawning
 #'  \item \emph{WeSt} - spawning stock weights
-#'  \item \emph{f} - fishing mortality
+#'  \item \emph{F} - selection pattern
 #'  \item \emph{WeCa} - catch weights
+#'  \item \emph{Fd} - selection pattern of discards
+#'  \item \emph{WeCad} - discard catch weights
+#'  \item \emph{Fi} - selection pattern of industrial waste
+#'  \item \emph{WeCai} - industrial catch weights
 #' }
-#' This part has not been tested yet.
 #' @param Flim A numeric value
 #' @param Fpa A numeric value
 #' @param Blim A numeric value
@@ -42,102 +79,124 @@
 #' @param Fmanagement A numeric value
 #' @param Bmanagement A numeric value
 #' @param RecruitmentLength A numeric value
-#' @param UnitsWeigths A character, e.g. "tonnes"
-#' @param UnitsRecruits A character, e.g. "millions"
+#' @param UnitsWeigths A character, expect any one of "kilograms", "tonnes",
+#' "thousand tonnes", "million tonnes" 
+#' @param UnitsRecruits A character, expect any one of "thousands", "millions",
+#' "billions"
 #' @param TypeLandings A character, e.g. "official", "ices estimates".
+#' @param CustomName1 XXX
+#' @param CustomName2 XXX
+#' @param CustomName3 XXX
+#' @param CustomName4 XXX
+#' @param CustomName5 XXX
+#' @param VersionStock XXX
+#' @param NameSystemProducedFile XXX
 rvk2icesxml <- function(rby,FishStock,AssessmentYear,RecruitmentAge,FAge,rba,UnitsWeigths="tonnes",
                         UnitsRecruits="millions",TypeLandings="official",
-                        Flim=0,Fpa=0,Blim=0,Bpa=0,
-                        FMSY=0,MSYBtrigger=0,Fmanagement=0,Bmanagement=0,RecruitmentLength=0) 
-  {
+                        Flim,Fpa,Blim,Bpa,FMSY,MSYBtrigger,
+                        Fmanagement,Bmanagement,RecruitmentLength,
+                        CustomName1,CustomName2,CustomName3,
+                        CustomName4,CustomName5,
+                        VersionStock,NameSystemProducedFile)
+{
   
   # Header data:
-  h1  <- "<?xml version='1.0' encoding='utf-8' standalone='no'?>\n"
-  h2  <- "<?xml-stylesheet type='text/xsl' href='StandrdGraphsStyle.xsl'?>\n"
-  h3  <- "<Assessment xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='ICES_Standard_Graphs.xsd'>\n"
-
-  h4  <- paste0("<FishStock>",FishStock,"</FishStock>\n")
-  h5  <- paste0("<AssessmentYear>",AssessmentYear,"</AssessmentYear>\n")
-  h6  <- paste0("<Flim>",Flim,"</Flim>\n")
-  h7  <- paste0("<Fpa>",Fpa,"</Fpa>\n")
-  h8  <- paste0("<Blim>",Blim,"</Blim>\n")
-  h9  <- paste0("<Bpa>",Bpa,"</Bpa>\n")
-  h10 <- paste0("<FMSY>",FMSY,"</FMSY>\n")
-  h11 <- paste0("<MSYBtrigger>",MSYBtrigger,"</MSYBtrigger>\n")
-  h12 <- paste0("<Fmanagement>",Fmanagement,"</Fmanagement>\n")
-  h13 <- paste0("<Bmanagement>",Bmanagement,"</Bmanagement>\n")
-  h14 <- paste0("<RecruitmentAge>",RecruitmentAge,"</RecruitmentAge>\n")
-  h14b <- paste0("<FAge>",FAge,"</FAge>\n")
-  h15 <- paste0("<RecruitmentLength>",RecruitmentLength,"</RecruitmentLength>\n")
-  h16 <- paste0("<UnitsWeigths>",UnitsWeigths,"</UnitsWeigths>\n")
-  h17 <- paste0("<UnitsRecruits>",UnitsRecruits,"</UnitsRecruits>\n")
-  h18 <- paste0("<TypeLandings>",TypeLandings,"</TypeLandings>\n")
+  h_xml <- "<?xml version='1.0' encoding='utf-8' standalone='no'?>\n"
+  h_xml <- paste0(h_xml,"<?xml-stylesheet type='text/xsl' href='StandrdGraphsStyle.xsl'?>\n")
+  h_xml <- paste0(h_xml,"<Assessment xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:noNamespaceSchemaLocation='ICES_Standard_Graphs.xsd'>\n")
+  h_xml <- paste0(h_xml,"<FishStock>",FishStock,"</FishStock>\n")
+  h_xml <- paste0(h_xml,"<AssessmentYear>",AssessmentYear,"</AssessmentYear>\n")
+  if(!missing(Flim)) h_xml <- paste0(h_xml,"<Flim>",Flim,"</Flim>\n")
+  if(!missing(Fpa))  h_xml <- paste0(h_xml,"<Fpa>",Fpa,"</Fpa>\n")
+  if(!missing(Blim)) h_xml <- paste0(h_xml,"<Blim>",Blim,"</Blim>\n")
+  if(!missing(Bpa))  h_xml <-  paste0(h_xml,"<Bpa>",Bpa,"</Bpa>\n")
+  if(!missing(FMSY)) h_xml <- paste0(h_xml,"<FMSY>",FMSY,"</FMSY>\n")
+  if(!missing(MSYBtrigger))    h_xml <- paste0(h_xml,"<MSYBtrigger>",MSYBtrigger,"</MSYBtrigger>\n")
+  if(!missing(Fmanagement))    h_xml <- paste0(h_xml,"<Fmanagement>",Fmanagement,"</Fmanagement>\n")
+  if(!missing(Bmanagement))    h_xml <- paste0(h_xml,"<Bmanagement>",Bmanagement,"</Bmanagement>\n")
+  if(!missing(RecruitmentAge)) h_xml <- paste0(h_xml,"<RecruitmentAge>",RecruitmentAge,"</RecruitmentAge>\n")
+  if(!missing(FAge))           h_xml <- paste0(h_xml,"<FAge>",FAge,"</FAge>\n")
+  if(!missing(RecruitmentLength)) h_xml <- paste0(h_xml,"<RecruitmentLength>",RecruitmentLength,"</RecruitmentLength>\n")
+  if(!missing(UnitsWeigths))   h_xml <- paste0(h_xml,"<UnitsWeigths>",UnitsWeigths,"</UnitsWeigths>\n")
+  if(!missing(UnitsRecruits))  h_xml <- paste0(h_xml,"<UnitsRecruits>",UnitsRecruits,"</UnitsRecruits>\n")
+  if(!missing(TypeLandings))   h_xml <- paste0(h_xml,"<TypeLandings>",TypeLandings,"</TypeLandings>\n")
   
-  header <- paste0(h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12,h13,h14,h14b,h15,h16,h17,h18)
-  
-  ## Stock summary data
-  # the wrappers
-  w0 <- c("<FishData>","</FishData>\n")
-  w1 <- c("<Year>","</Year>\n")
-  w2 <- c("<Recruitment>","</Recruitment>\n")
-  w3 <- c("<TBiomass>","</TBiomass>\n")
-  w4 <- c("<SSB>","</SSB>\n")
-  w5 <- c("<Landings>","</Landings>\n")
-  w6 <- c("<YieldSSB>","</YieldSSB>\n")
-  w7 <- c("<F_Landings>","</F_Landings>\n")
+  rby_names <- names(rby)
   
   # the year loop
-
   for (i in 1:nrow(rby)) {
-    tmp2 <- paste0(w0[1],
-                  w1[1],rby$year[i],w1[2],
-                  w2[1],rby$r[i],w2[2],
-                  w3[1],rby$bio[i],w3[2],
-                  w4[1],rby$ssb[i],w4[2],
-                  w5[1],rby$y[i],w5[2],
-                  w6[1],rby$y[i]/rby$ssb[i],w6[2],
-                  w7[1],rby$f[i],w7[2],
-                  w0[2])
+    x <- paste0("<FishData>\n","<Year>",rby$Year[i],"</Year>\n")
+    
+    if(any(match(rby_names,"Low_Recruitment"),na.rm=T)) x <- paste0(x,"<Low_Recruitment>",rby$Low_Recruitment[i],"</Low_Recruitment>\n")
+    if(any(match(rby_names,"Recruitment"),na.rm=T)) x <- paste0(x,"<Recruitment>",rby$Recruitment[i],"</Recruitment>\n")
+    if(any(match(rby_names,"High_Recruitment"),na.rm=T)) x <- paste0(x,"<High_Recruitment>",rby$High_Recruitment[i],"</High_Recruitment>\n")
+    
+    if(any(match(rby_names,"Low_TBiomass"),na.rm=T)) x <- paste0(x,"<Low_TBiomass>",rby$Low_TBiomass[i],"</Low_TBiomass>\n")
+    if(any(match(rby_names,"TBiomass"),na.rm=T)) x <- paste0(x,"<TBiomass>",rby$TBiomass[i],"</TBiomass>\n")
+    if(any(match(rby_names,"High_TBiomass"),na.rm=T)) x <- paste0(x,"<High_TBiomass>",rby$High_TBiomass[i],"</High_TBiomass>\n")
+    
+    if(any(match(rby_names,"Low_SSB"),na.rm=T)) x <- paste0(x,"<Low_SSB>",rby$Low_SSB[i],"</Low_SSB>\n")
+    if(any(match(rby_names,"SSB"),na.rm=T)) x <- paste0(x,"<SSB>",rby$SSB[i],"</SSB>\n")
+    if(any(match(rby_names,"High_SSB"),na.rm=T)) x <- paste0(x,"<High_SSB>",rby$High_SSB[i],"</High_SSB>\n")
+    
+    if(any(match(rby_names,"Catches"),na.rm=T)) x <- paste0(x,"<Catches>",rby$Catches[i],"</Catches>\n")
+    if(any(match(rby_names,"Landings"),na.rm=T)) x <- paste0(x,"<Landings>",rby$Landings[i],"</Landings>\n")
+    if(any(match(rby_names,"Discards"),na.rm=T)) x <- paste0(x,"<Discards>",rby$Discards[i],"</Discards>\n")
+    
+    if(any(match(rby_names,"IBC"),na.rm=T)) x <- paste0(x,"<IBC>",rby$IBC[i],"</IBC>\n")
+    if(any(match(rby_names,"Unallocated_Removals"),na.rm=T)) x <- paste0(x,"<Unallocated_Removals>",rby$Unallocated_Removals[i],"</Unallocated_Removals>\n")
+    if(any(match(rby_names,"YieldSSB"),na.rm=T)) x <- paste0(x,"<YieldSSB>",rby$YieldSSB[i],"</YieldSSB>\n")
+    
+    if(any(match(rby_names,"Low_F"),na.rm=T)) x <- paste0(x,"<Low_F>",rby$Low_F[i],"</Low_F>\n")
+    if(any(match(rby_names,"F"),na.rm=T)) x <- paste0(x,"<F>",rby$F[i],"</F>\n")
+    if(any(match(rby_names,"High_F"),na.rm=T)) x <- paste0(x,"<High_F>",rby$High_F[i],"</High_F>\n")
+    
+    if(any(match(rby_names,"F_Landings"),na.rm=T)) x <- paste0(x,"<F_Landings>",rby$F_Landings[i],"</F_Landings>\n")
+    if(any(match(rby_names,"F_Discards"),na.rm=T)) x <- paste0(x,"<F_Discards>",rby$F_Discards[i],"</F_Discards>\n")
+    if(any(match(rby_names,"F_IBC"),na.rm=T)) x <- paste0(x,"<F_IBC>",rby$F_IBC[i],"</F_IBC>\n")
+    if(any(match(rby_names,"F_Unallocated"),na.rm=T)) x <- paste0(x,"<F_Unallocated>",rby$F_Unallocated[i],"</F_Unallocated>\n")
+    
+    if(any(match(rby_names,"SoP"),na.rm=T)) x <- paste0(x,"<SoP>",rby$SoP[i],"</SoP>\n")
+    if(any(match(rby_names,"Custom1"),na.rm=T)) x <- paste0(x,"<Custom1>",rby$Custom1[i],"</Custom1>\n")
+    if(any(match(rby_names,"Custom2"),na.rm=T)) x <- paste0(x,"<Custom2>",rby$Custom2[i],"</Custom2>\n")
+    if(any(match(rby_names,"Custom3"),na.rm=T)) x <- paste0(x,"<Custom3>",rby$Custom3[i],"</Custom3>\n")
+    if(any(match(rby_names,"Custom4"),na.rm=T)) x <- paste0(x,"<Custom4>",rby$Custom4[i],"</Custom4>\n")
+    if(any(match(rby_names,"Custom5"),na.rm=T)) x <- paste0(x,"<Custom5>",rby$Custom5[i],"</Custom5>\n")
+    
+    x <- paste0(x,"</FishData>\n")
     if (i == 1) {
-      tmp <- tmp2
-      } else {
-        tmp <- paste0(tmp,tmp2)
-      }
+      rby_xml <- x
+    } else {
+      rby_xml <- paste0(rby_xml,x)
+    }
   }
   
   ## Yield per recruit input
   if(missing(rba)) {
-    return(paste0(header,tmp,"</Assessment>\n"))
+    return(paste0(h_xml,rby_xml,"</Assessment>\n"))
   } else {
-    # the wrappers
-    s0 <- c("<Sensitivity_Data>","</Sensitivity_Data>\n")
-    s1 <- c("<Age>","</Age>\n")
-    s2 <- c("<M>","</M>\n")
-    s3 <- c("<Mat>","</Mat>\n")
-    s4 <- c("<PF>","</PF>\n")
-    s5 <- c("<PM>","</PM>\n")
-    s6 <- c("<WeSt>","</WeSt>\n")
-    s7 <- c("<F>","</F>\n")
-    s8 <- c("<WeCa>","</WeCa>\n")
-    
+    rba_names <- names(rba)
     # the age loop
     for (i in 1:nrow(rba)) {
-      tmp2 <- paste0(s0[1],
-                     s1[1],rba$age[i],s1[2],
-                     s2[1],rba$M[i],s2[2],
-                     s3[1],rba$Mat[i],s3[2],
-                     s4[1],rba$PF[i],s4[2],
-                     s5[1],rba$PM[i],s5[2],
-                     s6[1],rba$WeSt[i],s6[2],
-                     s7[1],rba$f,s7[2],
-                     s8[1],WeCa[i],s8[2],
-                     s0[2])
+      x <- "<Sensitivity_Data>"
+      x <- paste0(x,"<Age>",rba$Age[i],"</Age>\n")
+      x <- paste0(x,"<M>",rba$M[i],"</M>\n")
+      x <- paste0(x,"<PF>",rba$PF[i],"</PF>\n")
+      x <- paste0(x,"<PM>",rba$PM[i],"</PM>\n")
+      x <- paste0(x,"<WeSt>",rba$WeSt[i],"</WeSt>\n")
+      x <- paste0(x,"<F>",rba$F[i],"</F>\n")
+      x <- paste0(x,"<WeCa>",rba$M[i],"</WeCa>\n")
+      if(any(match(rba_names,"Fd"),na.rm=T)) x <- paste0(x,"<Fd>",rba$Fd[i],"</Fd>\n")
+      if(any(match(rba_names,"WeCad"),na.rm=T)) x <- paste0(x,"<WeCad>",rba$WeCad[i],"</WeCad>\n")
+      if(any(match(rba_names,"Fi"),na.rm=T)) x <- paste0(x,"<Fi>",rba$Fi[i],"</Fi>\n")
+      if(any(match(rba_names,"WeCai"),na.rm=T)) x <- paste0(x,"<WeCai>",rba$WeCai[i],"</WeCai>\n")
+      x <- paste0(x,"</Sensitivity_Data>\n")
       if (i == 1) {
-        tmp3 <- tmp2
+        rba_xml <- x
       } else {
-        tmp3 <- paste0(tmp3,tmp2)
+        rba_xml <- paste0(rba_xml,x)
       }
     }
-    return(paste0(header,tmp,tmp3,"</Assessment>\n"))
+    return(paste0(h_xml,rby_xml,rba_xml,"</Assessment>\n"))
   }
 }
