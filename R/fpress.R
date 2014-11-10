adf887194fccc0c04457df4c21ec963f500a7305
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2014-11-10 07:26:37 +0000

    add srest from jose and ypr

--- R/fpress.R
+++ R/fpress.R
@@ -76,7 +76,11 @@ hcr_set_dimensions <- function(ctr) {
   d$cvcW <- hcr_set_wgtErrors(d$cvcW,ctr)
   d$cvsW <- d$cvcW
   #
-  d$cvR <- hcr_set_recErrors(d$cvR,ctr)
+  if(is.null(ctr$ssb_pars)) {
+    d$cvR <- hcr_set_recErrors(d$cvR,ctr)
+  } else {
+    d$cvR <- hcr_set_recErrors2(d$cvR,ctr)
+  }
   #
   d$assError <- hcr_set_assErrors(d$assError,ctr)
   
@@ -182,6 +186,36 @@ hcr_set_recErrors <- function(d,ctr)
   
   return(d)
 }
+
+#' @title HCR: Setup of recruitment error structure
+#' 
+#' @description Each iter has own cv and rho
+#' 
+#' @export
+#' 
+#' @param d XXX
+#' @param ctr XXX
+#' 
+hcr_set_recErrors2 <- function(d,ctr) 
+{
+  
+  n_years  <- dim(d)[1]
+  n_hrates <- dim(d)[2]
+  n_iters  <- dim(d)[3]
+  
+  # Recruitment: cv & autocorrelation
+  x <- array(rnorm(n_years * n_iters),
+             dim=c(n_years , n_iters))
+  
+  for (y in 2:n_years) x[y,] <- ctr$ssb_pars$r_rho * x[y-1,] + sqrt(1 - ctr$ssb_pars$r_rho^2) * x[y,]
+  
+  x <- exp(x*ctr$ssb_pars$r_cv)
+  for (h in 1:n_hrates) d[,h,] <- x
+  
+  return(d)
+}
+
+
 
 #' @title HCR: Reading starting years input from file
 #' 
